d02182f3e85e2e2e8dcef77b0035ed5eadaa793c
tmasters <tmasters@thoughtworks.com>
2017-04-12 17:06:12 +0100

    got some weather svgs

--- frontend/react-js/src/App.js
+++ frontend/react-js/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import sun from './28.svg';
 import './App.css';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {weather: []};
+    this.state = {weather: {}};
   }
 
   componentDidMount() {
@@ -17,7 +17,9 @@ class App extends Component {
     return fetch('http://127.0.0.1:5000/weather/london')
     .then(response => response.json())
     .then((responseJson) => {
-      console.log(responseJson)
+      this.setState({
+        weather: responseJson
+      })
     })
     .catch((error) => {
       console.error(error);
@@ -29,12 +31,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Reactive Weather</h2>
         </div>
         <p className="App-intro">
-          Hello world! Your weather is...
+          Hello world! Your weather in {this.state.weather.location}...is {this.state.weather.forecast} with a grand {this.state.weather.temperature}{this.state.weather.temperatureUnits}
         </p>
+        <img src={sun} className="sun" alt="sun" />
       </div>
     );
   }
